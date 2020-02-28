741eaecaa02a1d17d0ce275b93b8d7c505a25685
Mohamed ben amar <medxbenamar@gmail.com>
2020-02-28 11:02:19 +0100

    seperated lexer and parser grammars

--- src/main/antlr4/generate.sh
+++ src/main/antlr4/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-antlr4 Staf.g4 -package com.sparkit.staf -visitor
+antlr4 StafLexer.g4 -package com.sparkit.staf -visitor
 javac  -classpath /usr/local/lib/antlr-4.8-complete.jar Staf*.java
 #grun com.sparkit.staf.Staf staf_file -gui script.staf
