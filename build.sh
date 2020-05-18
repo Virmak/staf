37a4f24d04687540aef0002ecbfc7f1056e4463e
Mohamed ben amar <medxbenamar@gmail.com>
2020-05-18 17:38:59 +0100

    improved build

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 mvn clean package
 cd staf-frontend && ng build --prod && cd ..
 mkdir -p build
+rm -r build/dist
 cp -r staf-frontend/dist build/
 cp staf-api/target/staf-api-0.0.1-SNAPSHOT.jar build/
 cp docker-compose.yml build/
 cp staf-frontend/nginx.conf build/
 cp staf-frontend/Dockerfile build/front.Dockerfile
+cd build && zip -r build.zip ./*
--- staf-frontend/src/environments/environment.prod.ts
+++ staf-frontend/src/environments/environment.prod.ts
@@ -8,8 +8,8 @@ export const environment = {
     const urlArr = window.location.href.split('/');
     return 'ws://' + urlArr[2] + ':15674/ws'
   },
-  webSocket: () => {
+  webSocket: function() {
     const urlArr = window.location.href.split('/');
-    return urlArr[0] + '//' + window.location.href.split('/')[2].replace(/:\d+/, ':6500') + '/staf-ws'
+    return urlArr[0] + '//' + window.location.href.split('/')[2].replace(/:\d+/, ':6500') + '/staf-ws';
   }
 };
